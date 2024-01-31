f2d2b84d8e9a04afa9c9194fdc358ea9620dbe3d
stevenht19 <mstvdev19@gmail.com>
2024-01-31 16:07:22 -0500

    refactor header code

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom'
 
 export function Header() {
   const { user } = useSession()
-  console.log(user)
 
   const items: MenuProps['items'] = [
     {
@@ -42,26 +41,24 @@ export function Header() {
           <img src='/logo.png' alt='Logo de la empresa Peru Scale' width={205} height={46} />
         </Link>
         <div className='flex items-center gap-3'>
-          {
-            user ? (
+          {user ? (
+            <span className='leading-5 py-3'>
+              Hola, <strong className='block mt-0.5'>{`${user.nombres} ${user.apellidos?.split(' ')[0]}`}</strong>
+            </span>
+          ) : (
+            <Dropdown
+              menu={{ items }}
+              overlayStyle={{
+                width: 150
+              }}
+              placement='bottomRight'
+              arrow={{ pointAtCenter: true }}
+            >
               <span className='leading-5 py-3'>
-                Hola, <strong className='block mt-0.5'>{`${user.nombres} ${user.apellidos?.split(' ')[0]}`}</strong>
+                Hola, <strong className='block mt-0.5'>Inicie Sesión</strong>
               </span>
-            ) : (
-              <Dropdown
-                menu={{ items }}
-                overlayStyle={{
-                  width: 150
-                }}
-                placement='bottomRight'
-                arrow={{ pointAtCenter: true }}
-              >
-                <span className='leading-5 py-3'>
-                  Hola, <strong className='block mt-0.5'>Inicie Sesión</strong>
-                </span>
-              </Dropdown>
-            )
-          }
+            </Dropdown>
+          )}
           <Divider type={'vertical'} style={{ height: 30 }} />
           <span>Productos</span>
           <Divider type={'vertical'} style={{ height: 30 }} />
